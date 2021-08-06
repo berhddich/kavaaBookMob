fcac7b186dec6488063e2bf3a8a24880394c0cc3
berhddich <66327609+berhddich@users.noreply.github.com>
2021-08-06 17:11:49 +0100

    cmt

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   hmr: false,
-  apiBaseUrl: 'http://192.168.0.105:45455/',
+  apiBaseUrl: 'http://192.168.0.105:45456/',
   clientId: '414e7827a3884f68abc79f7283837fd1',};
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   hmr: false,
-  apiBaseUrl: 'http://192.168.0.105:45455/',
+  apiBaseUrl: 'http://192.168.0.105:45456/',
   clientId: '414e7827a3884f68abc79f7283837fd1',};
 
 /*
